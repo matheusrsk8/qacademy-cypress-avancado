e701c96ed737a6696f30bf49d29b77110d8b9905
matheusr_sk8 <matheusrocha.jobs@gmail.com>
2023-02-21 22:34:34 -0300

    Correçao elemento password

--- cypress/support/pages/resetPass/elements.js
+++ cypress/support/pages/resetPass/elements.js
@@ -1,5 +1,5 @@
 exports.el = {
-   title: ('h1', 'Resetar Senha'),
+   title: ('h1', 'Resetar senha'),
    pass: 'input[placeholder="Nova senha"]',
    confirmPass: 'input[placeholder="Confirmação da senha"]',
    btnChangePass: ('button', 'Alterar senha')
